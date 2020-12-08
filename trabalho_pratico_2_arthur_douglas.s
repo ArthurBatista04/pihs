c08b4b857d2ec0857a750c6786f1ace095d633bc
arthurbatista04 <arthur.batista@eurekalabs.com.br>
2020-12-08 11:39:00 -0300

    leitura e escrita em arq parcial

--- trabalho_pratico_2_arthur_douglas.s
+++ trabalho_pratico_2_arthur_douglas.s
@@ -38,6 +38,7 @@
 	pedecpf:	.asciz	"\nDigite o CPF: "
 	pedegenero:	.asciz	"\nDigite o genero <M>asculino/<F>eminino: "
 	pedereajuste: .asciz "\nDigite o percentual de reajuste: "
+	pedeNomeArq: .asciz "\nEntre com o nome do arquivo de entrada/saida\n> "
 	listvazia:	.asciz	"\nLISTA ESTÁ VAZIA!\n "
 	semnome:	.asciz	"\nNAO TEM REGISTRO COM ESSE NOME!\n "
 	pergmenu: .asciz	"\n\nMenu do usuário\n----------------\nDigite 1 p/ inserir\n2 p/ consultar\n3 p/ excluir\n4 p/ relatório\n5 p/ Reajuste salarial\n6 p/ carregar arq\n7 p/ gravar em arq\n0 p/ sair: "
@@ -71,13 +72,47 @@
 
 	lista:		.int	0
 	regist:		.int 	0
+	registro: 	.space  232
 	atual:		.int 	0
 	reajuste:	.double 0.0
 	cem:		.double 100.0
+	nomeArq:	.space  50
+	descritor: 	.int 	0 # descritor do arquivo de entrada/saida
 
 	opcao:		.int 	0
 
 	NULL:		.int	0
+
+	O_RDONLY: .int 0x0000 # somente leitura
+	O_WRONLY: .int 0x0001 # somente escrita
+	O_RDWR:   .int 0x0002 # leitura e escrita
+	O_CREAT:  .int 0x0040 # cria o arquivo na abertura, caso ele n�o exista
+	O_EXCL:   .int 0x0080 # for�a a cria��o
+	O_APPEND: .int 0x0400 # posiciona o cursor do arquivo no final, para adi��o
+	O_TRUNC:  .int 0x0200 # reseta o arquivo aberto, deixando com tamanho 0 (zero)
+
+
+	S_IRWXU: .int 0x01C0# user (file owner) has read, write and execute permission
+	S_IRUSR: .int 0x0100 # user has read permission
+	S_IWUSR: .int 0x0080 # user has write permission
+	S_IXUSR: .int 0x0040 # user has execute permission
+	S_IRWXG: .int 0x0038 # group has read, write and execute permission
+	S_IRGRP: .int 0x0020 # group has read permission
+	S_IWGRP: .int 0x0010 # group has write permission
+	S_IXGRP: .int 0x0008 # group has execute permission
+	S_IRWXO: .int 0x0007 # others have read, write and execute permission
+	S_IROTH: .int 0x0004 # others have read permission
+	S_IWOTH: .int 0x0002 # others have write permission
+	S_IXOTH: .int 0x0001 # others have execute permission
+	S_NADA:  .int 0x0000 # n�o altera a situa��o
+
+	SYS_EXIT: 	.int 1
+	SYS_FORK: 	.int 2
+	SYS_READ: 	.int 3
+	SYS_WRITE: 	.int 4
+	SYS_OPEN: 	.int 5
+	SYS_CLOSE: 	.int 6
+	SYS_CREAT: 	.int 8
 
 .section .text
 
@@ -117,9 +152,117 @@ menu:
 	# veja se quer reajustar salario
 	cmpl $5, %eax
 	je reajustar
+	# veja se quer ler arquivo
+	cmpl $6, %eax
+	je leitura
+	# veja se quer escrever arquivo
+	cmpl $7, %eax
+	je escrita
 	# veja se quer sair
 	cmpl $0, %eax
 	je _end
+# funcao que fecha o arq
+fechaArq:
+	movl 	SYS_CLOSE, %eax
+	movl 	descritor, %ebx # recupera o descritor
+	int 	$0x80
+	ret
+# funcao que realiza a leitura dos registros salvo em um arq
+leitura:
+	movl	$NULL, %eax
+	movl	%eax, lista # a lista é reinicializada com o endereço de null
+
+	pushl 	$pedeNomeArq
+	call 	printf
+	pushl 	$nomeArq # variavel que conterá o nome do arq
+	call 	gets
+	call 	gets
+	addl 	$8, %esp
+
+	call	abreArqE # abre o arquivo para leitura
+
+	call 	lerArq
+# funcao que vai realizar a leitura dos registros e salvar na lista
+lerArq:
+	movl 	SYS_READ, %eax # %eax retorna numero de bytes lidos
+	movl 	descritor, %ebx # recupera o descritor
+	movl 	$registro, %ecx
+	movl 	tamreg, %edx
+	int 	$0x80 # le registro do arquivo
+	
+	cmpl 	$0, %eax # veja se chegou no final do arq
+	
+	je 		fechaEMenu
+
+	movl	lista, %ecx # conteudo primeiro_elemento da lista
+	movl	registro, %edi # novo elemento a ser inserido
+	movl	$NULL, %ebx
+	call 	insere_lista # insere o conteudo que está em edi na lista
+
+	jmp 	lerArq
+# fecha o arquivo e volta para o menu
+fechaEMenu:
+	call 	fechaArq
+	jmp		menu
+# abre o arquivo para leitura
+abreArqE:
+	movl 	SYS_OPEN, %eax 	# system call OPEN: retorna o descritor em %eax
+	movl 	$nomeArq, %ebx
+	movl 	O_RDONLY, %ecx
+	int 	$0x80
+	movl 	%eax, descritor # guarda o descritor
+	ret
+
+# funcao que realiza a escrita de todos os registros em um arq
+escrita:
+
+	pushl 	$pedeNomeArq
+	call 	printf
+	pushl 	$nomeArq # variavel que conterá o nome do arq
+	call 	gets
+	call 	gets
+	addl 	$8, %esp
+
+	call	abreArq # abre o arquivo
+
+	movl	$lista, %edi # endereco do primeiro elemento da lista em edi
+	movl	$NULL, %ebx # endereco do null
+
+	cmpl  	$NULL, (%edi) # verifica se o endereco do null eh igual ao do primeiro elemento da lsita
+	je		lista_vazia
+# escreve cada registro ate chegar no final da lista
+loopescrita:
+	movl	$NULL, %ebx # endereco do null
+	cmpl  	(%edi), %ebx # lista->proximo == endereco do null	
+	jne		gravaReg
+
+	call	fechaArq # fecha o arq
+	jmp		menu
+
+
+# funcao que vai abrir o arq para escrita (sempre sobreescreve o arq antigo, caso exista)
+abreArq:
+	movl 	SYS_OPEN, %eax 	# system call OPEN: retorna o descritor em %eax
+	movl 	$nomeArq, %ebx
+	movl 	O_WRONLY, %ecx
+	orl 	O_CREAT, %ecx
+	orl 	O_TRUNC, %ecx
+	movl 	S_IRUSR, %edx
+	orl 	S_IWUSR, %edx
+	int 	$0x80
+	movl 	%eax, descritor # guarda o descritor
+	ret
+# funcao que vai escrever o reg no arq
+gravaReg:
+	movl	%edi, %ecx 	# move o endereco que edi está apontando para ecx
+	movl 	SYS_WRITE, %eax # abre o arquivo p/ escrita
+	movl 	descritor, %ebx # recupera o descritor
+	movl 	tamreg, %edx # tam do registro 232
+	int 	$0x80
+	
+	movl	(%edi), %edi # recupera o conteudo que edi está apontando e coloca em edi
+	addl	$228, %edi # avanca pro proximo registro, 228 => campo do proximo
+	jmp 	loopescrita
 
 # funcao que realiza reajuste salarial
 reajustar:
